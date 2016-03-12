1f6e861664f8bb791ea8dc465162acd623441d52
Nolan Nichols <nolan.nichols@gmail.com>
2016-03-12 15:24:37 -0800

    add: logging

--- sibis/__init__.py
+++ sibis/__init__.py
@@ -1,23 +1,7 @@
-#!/usr/bin/env python
-"""
+##
+##  Copyright 2016 SRI International
+##  See COPYING file distributed along with the package for the copyright and license terms
+##
 
-"""
-__author__ = 'Nolan Nichols <https://orcid.org/0000-0003-1099-3328>'
-import os
-import sys
-
-
-def main(args=None):
-    pass
-
-
-if __name__ == "__main__":
-    import argparse
-
-    formatter = argparse.RawDescriptionHelpFormatter
-    default = 'default: %(default)s'
-    parser = argparse.ArgumentParser(prog="file_name.py",
-                                     description=__doc__,
-                                     formatter_class=formatter)
-    argv = parser.parse_args()
-    sys.exit(main(args=argv))
+import logger
+logging = logger.Logging()
---
+++ sibis/logger.py
@@ -0,0 +1,28 @@
+##
+##  Copyright 2016 SRI International
+##  See COPYING file distributed along with the package for the copyright and license terms
+##
+
+import json
+import logging
+import collections
+
+
+class Logging(object):
+    """
+    SIBIS Logging Module
+    """
+    def __init__(self):
+        self.logging = logging
+        self.logging.basicConfig(level=logging.INFO, format='%(message)s')
+        self.log = collections.OrderedDict()
+
+    def info(self, uid, message, **kwargs):
+        """
+        Relpaces logging.info
+        """
+        self.log.update(experiment_site_id=uid,
+                        error=message)
+        self.log.update(kwargs)
+        log = json.dumps(self.log)
+        return self.logging.info(log)
