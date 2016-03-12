74bda1366788a3331ff9dbaa80d5c0cb25233cbc
Nolan Nichols <nolan.nichols@gmail.com>
2016-03-12 10:27:45 -0800

    add: setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sibis',
+      version='0.0.1',
+      description='Scalable Informatics for Biomedical Imaging Studies',
+      url='http://github.com/sibis-platform/sibis',
+      author='Nolan Nichols',
+      author_email='nolan.nichols@gmail.com',
+      license='BSD',
+      packages=['sibis'],
+      zip_safe=False)
