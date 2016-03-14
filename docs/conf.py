ba81ff0cc846efc6eb632c2b1368bd27a00284f1
Nolan Nichols <nolan.nichols@gmail.com>
2016-03-13 21:29:23 -0700

    doc: not building yet

--- docs/conf.py
+++ docs/conf.py
@@ -25,10 +25,13 @@ github_doc_root = 'https://github.com/sibis-platform/sibis/tree/master/'
 
 
 def setup(app):
-    app.add_config_value('recommonmark_config', {
-            'url_resolver': lambda url: github_doc_root + url,
-            'auto_toc_tree_section': 'Contents',
-            }, True)
+    app.add_config_value('recommonmark_config',
+                         {'url_resolver': lambda url: github_doc_root + url,
+                          'auto_toc_tree_section': 'Contents',
+                          'enable_auto_doc_ref': True,
+                          'enable_eval_rst': True,
+                          'enable_auto_toc_tree': True},
+                         True)
     app.add_transform(AutoStructify)
 
 source_parsers = {
