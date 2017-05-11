a2ce85d5af6198fd065d72ccf8f9940dd52b534f
Kilian Pohl <torsten@ncanda-storage.cluster>
2017-05-11 08:40:42 -0700

    Timer included and works

--- sibislogger.py
+++ sibislogger.py
@@ -7,8 +7,9 @@ import sys
 import json
 import logging
 import collections
+import time 
+import os
 import post_issues_to_github as pig
-
 # set logger of python packages to warning so that we avoid info messages being printed out 
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 logging.getLogger("requests").setLevel(logging.WARNING)
@@ -25,6 +26,9 @@ class sibisLogging():
         self.postGithubRepo = None
         self.postGithubTitle = None
         self.postGithubLabel = None
+        self.startTime1=None
+        self.startTime2=None
+        self.fileTime=None
         self.verbose = False
 
     def info(self, uid, message, **kwargs):
@@ -75,12 +79,71 @@ class sibisLogging():
 
         return True
 
-def init_log(verbose=False,post_to_github=False,github_issue_title="",github_issue_label=""):
+    def startTimer1(self,timerDir,timerFile):
+        if not os.path.exists(timerDir): 
+            self.info("sibislogger.py","Error: Directory  " + timerDir + " does not exist - timer disabled") 
+        else : 
+            self.fileTime = os.path.join(timerDir, timerFile)  
+            self.startTime1 = time.time()
+
+    def startTimer2(self):
+        if self.fileTime  :
+            self.startTime2 = time.time()
+
+    def _stopTimerGeneral(self,timerID,msg=None):
+        if not self.fileTime : 
+            return 
+            
+        if timerID == 1: 
+            startTimer= self.startTime1
+        else :
+            startTimer= self.startTime2 
+        
+        if not startTimer: 
+            return
+
+        endTimer=time.time()
+        time_date_format = '%Y-%m-%d %H:%M:%S'
+        time_diff = int(1000*(endTimer - startTimer))
+        time_diff_sec = int(time_diff / 1000)
+        timeLine= ','.join([time.strftime(time_date_format,time.localtime(startTimer)),time.strftime(time_date_format,time.localtime(endTimer)),str(time_diff_sec/60) + ":" + str(time_diff_sec%60).zfill(2)  + ":" + str(time_diff%1000).zfill(3)])
+        timeLine += ','
+        if msg : 
+            timeLine += '"' + str(msg) + '"'
+
+        try :
+            if os.path.isfile(self.fileTime) :
+                fd = open(self.fileTime,'a')
+            else :
+                fd = open(self.fileTime,'w')
+                fd.write("start-time,end-time,difference(min:sec:msec),message\n")
+
+            fd.write(timeLine +'\n')
+            fd.close()
+            
+
+        except Exception as err_msg: 
+            self.info('sibislogger','Error: Failed to write to time stamp to file',
+                     fileTime=self.fileTime,
+                     err_msg=str(err_msg)) 
+
+    def takeTimer1(self,msg=None):
+        self._stopTimerGeneral(1,msg)
+
+
+    def takeTimer2(self,msg=None):
+        self._stopTimerGeneral(2,msg)
+
+
+def init_log(verbose=False,post_to_github=False,github_issue_title="",github_issue_label="",timerDir=None):
     global log
     log = sibisLogging()
     log.verbose = verbose
     if post_to_github: 
         log.post_to_github(github_issue_title, github_issue_label)
+    
+    if timerDir : 
+        log.startTimer1(timerDir,github_issue_label + "-time_log.csv")
 
 # if this fails bc it cannot find log, please make sure init_log is called firs 
 def info(uid, message, **kwargs):
--- tests/test_sibislogger.py
+++ tests/test_sibislogger.py
@@ -5,29 +5,59 @@
 ##  See COPYING file distributed along with the package for the copyright and license terms
 ##
 
-from sibisAlpha import sibislogger
-from sibisAlpha import post_issues_to_github as pig
+from sibispy import sibislogger
+from sibispy import post_issues_to_github as pig
+import os 
+import sys
+import time 
 
 log = sibislogger.sibisLogging()
+
+if os.path.isfile('/tmp/test_sibislogger-time_log.csv') : 
+   os.remove('/tmp/test_sibislogger-time_log.csv') 
+
+log.takeTimer1()
+log.startTimer1('/asaffsaf','test_sibislogger-time_log.csv')
+if log.fileTime :
+   sys.exit(1)
+
+log.startTimer1('/tmp','test_sibislogger-time_log.csv') 
+if not log.fileTime :
+   sys.exit(1)
+
+time.sleep(1)
+log.takeTimer1('should be 0:01:001')
+if not os.path.isfile('/tmp/test_sibislogger-time_log.csv') :
+   sys.exit(1)
+
 log.verbose = True
+
 # just normal 
+
 print "============== Test 1 ==================="
 print "Push issue to logger"
 blub = log.info('sibislogger_test_1',"Unit Testing 1", msg="Please ignore message")
+
 # find out how to close issues from api 
 # post to github 
 print "============== Test 2 ==================="
 print "Post issue on GitHub"
+log.startTimer2()
 log.post_to_github('Unit Test', 'testing')
+log.takeTimer2("Test 2: Post Github: Initialize")
 
 iID="sibislogger_test_2"
 iTitle= "Testing 2"
 
 print "Issue exists:",
+log.startTimer2()
 issue=pig.get_issue(log.postGithubRepo, iID + ", " +  iTitle, False)
+log.takeTimer2("Test 2: Post Github: Get Issue")
 if issue:
     print "yes, closed"
+    log.startTimer2()
     issue.edit(state='close')
+    log.takeTimer2("Test 2: Post Github: Change State")
 else : 
     print "No" 
 
@@ -43,5 +73,7 @@ if issue:
 else : 
     print "Error: Could not find issue!"
  
+log.takeTimer1()
+print "Output of time log written to "  + log.fileTime 
 
 # log.info('Unit Test (testing)', 'sibislogger_test_2 (TESTING)',"Testing 2", msg="Please ignore message")
