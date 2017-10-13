e29b11210e9ac4a7b519fc46d2c500e883d09f2a
Kilian Pohl <torsten@ncanda-storage.cluster>
2017-10-13 12:52:05 -0700

    Removed small bug

--- tests/test_check_dti_gradients.py
+++ tests/test_check_dti_gradients.py
@@ -25,7 +25,7 @@ assert(session.configure(config_file))
 check = chk.check_dti_gradients()
 if not check.configure(session,check_decimals=2) :
     print "If it fails bc it cannot find ground truth files please execute ../cmd/download_dti_groundtruth.py"
-    syst.exit(1)
+    sys.exit(1)
 
 gt_path_dict = check.get_ground_truth_gradient_path_dict()
 assert(len(gt_path_dict))
