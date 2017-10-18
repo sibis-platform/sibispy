e8148bb13a8aa11226e950a1de7acfe8011122a4
Michael Hasak <mphasak@gmail.com>
2017-10-18 16:29:45 -0700

    seens now to work - testing in production enviornment

--- redcap_to_casesdir.py
+++ redcap_to_casesdir.py
@@ -285,7 +285,7 @@ class redcap_to_casesdir(object):
 
     # NCANDA SPECIFIC - Generalize later
     # Create "demographics" file "by hand" - this includes some text fields
-    def export_demographics(self,subject,subject_code,arm_code,visit_code,site,visit_age,subject_data,visit_data,measures_dir,verbose=False) :
+    def export_subject_demographics(self,subject,subject_code,arm_code,visit_code,site,visit_age,subject_data,visit_data,measures_dir,verbose=False) :
             # Latino and race coding arrives here as floating point numbers; make
             # int strings from that (cannot use "int()" because it would fail for
             # missing data
@@ -329,102 +329,123 @@ class redcap_to_casesdir(object):
 
             return sibis_utils.safe_dataframe_to_csv(pandas.DataFrame(series).T,os.path.join(measures_dir, 'demographics.csv'),verbose=verbose)
 
-    # Export selected REDCap data to cases dir 
-    def export(self,redcap_project, site, subject, event, subject_data, visit_age, visit_data, arm_code, visit_code, subject_code, subject_datadir,forms_this_event, select_exports=None, verbose=False):
+    def export_subject_form(self, export_name, subject, subject_code, arm_code, visit_code, all_records, measures_dir,verbose = False):
+        # Remove the complete field from the list of forms
+        complete = '{}_complete'.format(self.__import_forms.get(export_name))
+        fields = [column for column in self.__export_forms.get(export_name)
+                  if column != complete]
 
-        # Mark subjects/visits that have QA completed by creating a hidden marker
-        # file
-        qafile_path = os.path.join(subject_datadir, '.qacomplete')
-        if visit_data['mri_qa_completed'] == '1':
-            try:
-                if not os.path.exists(qafile_path):
-                    qafile = open(qafile_path, 'w')
-                    qafile.close()
-            except IOError as error:
-                print("ERROR: unable to open QA marker file in {}. {}".format(subject_datadir, error))
-        else:
-            try:
-                if os.path.exists(qafile_path):
-                    os.remove(qafile_path)
-            except OSError as error:
-                print("ERROR: unable to remove QA marker file {}. {}".format(qafile_path, error))
+        # Select data for this form - "reindex_axis" is necessary to put
+        # fields in listed order - REDCap returns them lexicographically sorted
+        fields = [i for i in fields if i not in ['subject', 'arm', 'visit']]
+        record = all_records[fields].reindex_axis(fields, axis=1)
+
+        # if I read it correctly then this statement is not possible 
+        if len(record) > 1:
+            slog.info(subject + visit_code, "ERROR: muliple records for that visit found for form '" + export_name + "'!" )
+            return None
+
+        # Nothing to do 
+        if not len(record):
+            if verbose :
+                slog.info(subject + visit_code, "Info: visit data did not contain records of form '" + export_name + "'!" )
+                
+            return None
+                
+
+        # First, add the three index columns
+        record.insert(0, 'subject', subject_code)
+        record.insert(1, 'arm', arm_code)
+        record.insert(2, 'visit', visit_code)
+
+        field_idx = 0
+        output_fields = []
+        for field in record.columns:
+            # Rename field for output if necessary
+            if field in self.__export_rename[export_name].keys():
+                output_field = self.__export_rename[export_name][field]
+            else:
+                output_field = field
+            output_fields.append(output_field)
+
+            # If this is an "age" field, truncate to 2 digits for privacy
+            if re.match('.*_age$', field):
+                record[field] = record[field].apply(self.__truncate_age__)
+
+            # If this is a radio or dropdown field
+            # (except "FORM_[missing_]why"), add a separate column for the
+            # coded label
+            if field in self.__code_to_label_dict.keys() and not re.match('.*_why$', field):
+                code = str(record[field].ix[0])
+                label = ''
+                if code in self.__code_to_label_dict[field].keys():
+                    label = self.__code_to_label_dict[field][code]
+                field_idx += 1
+                record.insert(field_idx, output_field + '_label', label)
+                output_fields.append(output_field + '_label')
+
+            field_idx += 1
+
+        # Apply renaming to columns
+        record.columns = output_fields
+
+        # Figure out path for CSV file and export this record
+        return sibis_utils.safe_dataframe_to_csv(record,os.path.join(measures_dir, export_name + '.csv'),verbose=verbose)
+
+    # First get data for all fields across all forms in this event - this
+    # speeds up transfers over getting each form separately
+    def get_subject_specific_form_data(self,subject,event,forms_this_event, redcap_project,select_exports=None):
+        # define fields and forms to export
+        all_fields = ['study_id']
+        export_list = []
+        for export_name in self.__export_forms.keys():
+            if (self.__import_forms[export_name] in forms_this_event): 
+                if (not select_exports or export_name in select_exports):
+                    all_fields += [re.sub('___.*', '', field_name) for field_name in self.__export_forms[export_name]]
+                    export_list.append(export_name)
+
+        # Get data
+        all_records = redcap_project.export_records(fields=all_fields,records=[subject], events=[event],format='df')
+
+        # return results 
+        return (all_records,export_list)
+
+    # Export selected REDCap data to cases dir 
+    def export_subject_all_forms(self,redcap_project, site, subject, event, subject_data, visit_age, visit_data, arm_code, visit_code, subject_code, subject_datadir,forms_this_event, select_exports=None, verbose=False):
+        # Do not really use this feature later 
+        # Mark subjects/visits that have QA completed by creating a hidden marker file
+        # qafile_path = os.path.join(subject_datadir, '.qacomplete')
+        # if visit_data['mri_qa_completed'] == '1':
+        #    try:
+        #        if not os.path.exists(qafile_path):
+        #            qafile = open(qafile_path, 'w')
+        #            qafile.close()
+        #    except IOError as error:
+        #        slog.info(subject_code,"ERROR: unable to open QA marker file in {}. {}".format(subject_datadir, error))
+        # else:
+        #    try:
+        #        if os.path.exists(qafile_path):
+        #            os.remove(qafile_path)
+        #    except OSError as error:
+        #        print("ERROR: unable to remove QA marker file {}. {}".format(qafile_path, error))
 
         # Check if the "measures" subdirectory already exists - this is where all
         # the csv files go. Create it if necessary.
+
         measures_dir = os.path.join(subject_datadir, 'measures')
         if not os.path.exists(measures_dir):
             os.makedirs(measures_dir)
 
         # Export demographics (if selected)
         if not select_exports or 'demographics' in select_exports:
-            self.export_demographics(subject,subject_code,arm_code,visit_code,site,visit_age,subject_data,visit_data,measures_dir,verbose)
+            self.export_subject_demographics(subject,subject_code,arm_code,visit_code,site,visit_age,subject_data,visit_data,measures_dir,verbose)
 
-        # First get data for all fields across all forms in this event - this
-        # speeds up transfers over getting each form separately
-        all_fields = ['study_id']
-        export_list = []
-        for export_name in export_forms.keys():
-            if (import_forms[export_name] in forms_this_event) and (not select_exports or export_name in select_exports):
-                all_fields += [re.sub('___.*', '', field_name) for field_name in export_forms[export_name]]
-                export_list.append(export_name)
-
-        all_records = redcap_project.export_records(fields=all_fields,
-                                                    records=[subject],
-                                                    events=[event],
-                                                    format='df')
-
+        (all_records,export_list) = self.get_subject_specific_form_data(subject,event,forms_this_event, redcap_project, select_exports)
         # Now go form by form and export data
         for export_name in export_list:
-            # Remove the complete field from the list of forms
-            complete = '{}_complete'.format(import_forms.get(export_name))
-            fields = [column for column in export_forms.get(export_name)
-                      if column != complete]
+            self.export_subject_form(export_name, subject, subject_code, arm_code, visit_code, all_records, measures_dir,verbose)
+            
 
-            # Select data for this form - "reindex_axis" is necessary to put
-            # fields in listed order - REDCap returns them lexicographically sorted
-            fields = [i for i in fields if i not in ['subject', 'arm', 'visit']]
-            record = all_records[fields].reindex_axis(fields, axis=1)
-
-            if len(record) == 1:
-                # First, add the three index columns
-                record.insert(0, 'subject', subject_code)
-                record.insert(1, 'arm', arm_code)
-                record.insert(2, 'visit', visit_code)
-
-                field_idx = 0
-                output_fields = []
-                for field in record.columns:
-                    # Rename field for output if necessary
-                    if field in export_rename[export_name].keys():
-                        output_field = export_rename[export_name][field]
-                    else:
-                        output_field = field
-                    output_fields.append(output_field)
-
-                    # If this is an "age" field, truncate to 2 digits for privacy
-                    if re.match('.*_age$', field):
-                        record[field] = record[field].apply(self.__truncate_age__)
-
-                    # If this is a radio or dropdown field
-                    # (except "FORM_[missing_]why"), add a separate column for the
-                    # coded label
-                    if field in code_to_label_dict.keys() and not re.match(
-                            '.*_why$', field):
-                        code = str(record[field].ix[0])
-                        label = ''
-                        if code in code_to_label_dict[field].keys():
-                            label = code_to_label_dict[field][code]
-                        field_idx += 1
-                        record.insert(field_idx, output_field + '_label', label)
-                        output_fields.append(output_field + '_label')
-
-                    field_idx += 1
-
-                # Apply renaming to columns
-                record.columns = output_fields
-
-                # Figure out path for CSV file and export this record
-                sibis_utils.safe_dataframe_to_csv(record,os.path.join(measures_dir, export_name + '.csv'),verbose=verbose)
 
     # What Arm and Visit of the study is this event?
     def translate_subject_and_event( self, subject_code, event_label):
@@ -441,4 +462,8 @@ class redcap_to_casesdir(object):
              date_format_ymd = self.__date_format_ymd
 
         return (datetime.datetime.strptime( date_to_str, date_format_ymd ) - datetime.datetime.strptime( date_from_str, date_format_ymd ) ).days
+
+    #def get_import_forms(self):
+    #    return self.__import_forms
+
 
--- tests/test_redcap_to_casesdir.py
+++ tests/test_redcap_to_casesdir.py
@@ -38,10 +38,6 @@ if not os.path.exists(outdir) :
 # Test creating data dictionaries
 assert(red2cas.create_demographic_datadict(outdir))
 
-# Test creating another form dictionary 
-form_name = red2cas.get_export_form_names()[0]
-assert(red2cas.create_datadict(form_name,outdir))
-
 #
 # Test writing out a case specific demographic file 
 # NCANDA SPECIFIC
@@ -65,27 +61,46 @@ visit_date = str(subject_visit_data['visit_date'])
 # subject_red_visit_id =  subject_red_id + "-" + visit_date
 subject_xnat_id =  str(subject_visit_data['mri_xnat_sid'])
 
-baseline_events = ['baseline_visit_arm_1','baseline_visit_arm_4']
+
 subject_fields = ['study_id', 'dob',  'exclude', 'enroll_exception',
                   'siblings_enrolled', 'siblings_id1', 'hispanic', 'race',
                   'race_other_code']
+#baseline_events = ['baseline_visit_arm_1','baseline_visit_arm_4']
+#for speed up just the single subject- records flag 
+baseline_events = ['baseline_visit_arm_1']
 subject_data = redcap_project.export_records(fields=subject_fields,
                                              events=baseline_events,
                                              event_name='unique',
+                                             records = [subject_red_id],
                                              format='df')
 subject_data = pandas.concat([subject_data.xs(event, level=1) for event in baseline_events])
-
-
 this_subject_data = subject_data.ix[subject_red_id]
+
 subject_dob_str = str( this_subject_data['dob'] )
 
 visit_age = red2cas.days_between_dates(subject_dob_str, visit_date ) / 365.242
 
 (arm_code,visit_code,subject_datadir_rel) = red2cas.translate_subject_and_event(subject_xnat_id, subject_event_id)
-assert(red2cas.export_demographics(subject_red_id, subject_xnat_id,arm_code,visit_code,subject_site_id, visit_age , this_subject_data, subject_visit_data, outdir))
+assert(red2cas.export_subject_demographics(subject_red_id, subject_xnat_id,arm_code,visit_code, subject_site_id, visit_age , this_subject_data, subject_visit_data, outdir))
+
+#
+# Test creating another form dictionary 
+#
+form_event_mapping = redcap_project.export_fem( format='df' )
+forms_this_event = set( form_event_mapping[form_event_mapping['unique_event_name'] ==  subject_event_id ]['form_name'].tolist() )
+form_name = next(iter(forms_this_event))
+assert(red2cas.create_datadict(form_name,outdir))
 
 #
 # Test writing out a case specific measurement file 
 # 
+
+
+(all_records,export_list) = red2cas.get_subject_specific_form_data(subject_red_id, subject_event_id, forms_this_event, redcap_project, select_exports=[form_name])
+assert((not all_records.empty) and export_list)
+
+assert(red2cas.export_subject_form(form_name, subject_red_id, subject_xnat_id,arm_code,visit_code, all_records, outdir, verbose=True))
+
+red2cas.export_subject_all_forms(redcap_project,  subject_site_id, subject_red_id, subject_event_id, this_subject_data, visit_age, subject_visit_data, arm_code, visit_code, subject_xnat_id, outdir,forms_this_event)
 
 print "Wrote results to " + outdir
