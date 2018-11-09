6f3dad1dd5cdf252aeff58c6d5fbd3eab97cc175
Jim Klo <jim.klo@sri.com>
2018-11-09 07:24:18 -0800

    Add new sibispy.svn module to wrap svn functionality
    since pysvn and svn modules don't work the same, creating an subversion wrapper module that provides the functionality we need regardless of the way each library works.

---
+++ svn.py
@@ -0,0 +1,99 @@
+##
+##  Copyright 2018 SRI International
+##  See COPYING file distributed along with the package for the copyright and license terms
+##
+
+
+from __future__ import absolute_import, print_function
+from svn.local import LocalClient
+
+
+class SibisSvnUpdate():
+  """
+  Changelog status performed by an SVN update.
+
+  Attributes:
+    target    (str):      The file or dirctory where svn update was performed.
+    revision  (int):      The current revision of the working directory
+    actions   (set(set)): The set of (action, filename) performed by the update.
+  """
+
+  class ActionTypes():
+    """
+    SVN 1.8 Update action types.
+    """
+    added = 'A'
+    broken_lock = 'B'
+    deleted = 'D'
+    updated = 'U'
+    conflicted = 'C'
+    merged = 'G'
+    existed = 'E'
+
+  def __init__(self, cmd_output=[]):
+    """
+    Constructor for SibisSvnUpdate.
+
+    Parameters:
+      cmd_output (list(str)): raw command output from an `svn update`. each element 
+                              in the list represents one line of output.
+    """
+    if len(cmd_output) >= 2:
+      header = cmd_output[0]
+      body = cmd_output[1:-1]
+      footer = cmd_output[-1]
+
+      ## get the file or folder updated
+      ts = header.index(r"'")+1
+      te = header.rindex(r"'")
+      self.target = header[ts:te]
+
+      ## get the version
+      fs = footer.rindex(r' ')+1
+      fe = footer.rindex(r'.')
+      self.revision = int(footer[fs:fe])
+
+      self.actions = set()
+      for line in body:
+        action, path = line.split(None, 1)
+        self.actions.add((action, path))
+      
+
+class SibisSvnClient(LocalClient):
+  """
+  Subversion client wrapper around svn module so we can normalize functionality
+  when changing svn libraries to something more portable than Tigris' pysvn.
+
+  Assumes that the path provided is an existing svn working directory.
+  """
+  
+  def __init__(self, path_, *args, **kwargs):
+    """
+    Constructor for SibisSvnClient class.
+
+    Parameters:
+      path_     (str):  path to svn working directory
+      username  (str):  [optional] svn repository username
+      password  (str):  [optional] svn repository password
+    """
+    super(SibisSvnClient, self).__init__(path_, *args, **kwargs)
+
+  def update(self, rel_filepaths=[], revision=None):
+    """
+    Update the current working directory to current revision.
+
+    Parameters:
+      rel_filepaths (list(str)):    list of relative subpaths to update
+      revision      (int or str):   The revision number to update to. None updates to HEAD. Default: None
+
+    Returns: SibisSvnUpdate
+    """
+    cmd = []
+    if revision is not None:
+      cmd += ['-r', str(revision)]
+    cmd += rel_filepaths
+    cmd_out = self.run_command(
+      'update',
+      cmd,
+      wd=self.path)
+    return SibisSvnUpdate(cmd_out)
---
+++ tests/test_sibis_svn.py
@@ -0,0 +1,110 @@
+##
+##  Copyright 2018 SRI International
+##  See COPYING file distributed along with the package for the copyright and license terms
+##
+
+"""
+Unit tests for sibispy.svn module.
+"""
+
+from __future__ import absolute_import, print_function
+
+import pytest
+from pytest_shutil.workspace import Workspace
+from path import Path
+from datetime import datetime
+from ..svn import SibisSvnClient
+import tempfile
+
+# pytest_plugins = ['pytest_svn']
+
+def ensure_path(svn_repo, repo_name, rel_file):
+  path = svn_repo.workspace / repo_name
+  split_path = rel_file.rsplit('/',1)
+  if len(split_path) == 2:
+    parent_path = path / split_path[0]
+    if not parent_path.exists():
+      parent_path.makedirs_p()
+      svn_repo.run("svn add {}".format(split_path[0]), cd=path)
+  else:
+    path.makedirs_p()
+  return path
+
+def del_file(svn_repo, repo_name, rel_file):
+  path = ensure_path(svn_repo, repo_name, rel_file)
+  txt_file = path / Path(rel_file)
+  msg = "Deleted File {} @ {}".format(txt_file, datetime.now())
+  svn_repo.run("svn delete {}".format(txt_file), cd=path)
+  return msg
+
+
+def mod_file(svn_repo, repo_name, rel_file):
+  path = ensure_path(svn_repo, repo_name, rel_file)
+  txt_file = path / Path(rel_file)
+  file_exists = txt_file.exists()
+  msg = "Modified File {} @ {}".format(txt_file, datetime.now())
+  txt_file.write_text(msg+'\n', append=True)
+
+  if (not file_exists):
+    svn_repo.run("svn add {}".format(txt_file), cd=path)
+  return msg
+
+mock_repo_commits = [
+  ({ ('A', 'file1'), ('A', 'file2'), ('A', 'file3') }, set()),
+  ({ ('D', 'file1'), ('U', 'file2'), ('A', 'file4') }, set()),
+  ({ ('A', 'file1'), ('U', 'file2'), ('U', 'file4') }, set()),
+  ({ ('A', 'alpha/file5'), ('A', 'alpha/file6'), ('A', 'alpha/file7') }, set([('A', 'alpha')])),
+  ({ ('D', 'alpha/file5'), ('U', 'alpha/file6'), ('U', 'alpha/file7') }, set()),
+  ({ ('A', 'alpha/file5'), ('U', 'alpha/file6'), ('D', 'alpha/file7') }, set()),
+  ({ ('A', 'beta/file5'), ('A', 'beta/file6'), ('A', 'beta/file7') }, set([('A', 'beta')])),
+  ({ ('D', 'beta/file5'), ('U', 'beta/file6'), ('U', 'beta/file7') }, set()),
+  ({ ('A', 'beta/file5'), ('U', 'beta/file6'), ('D', 'beta/file7') }, set())
+]
+
+repo_actions = {
+  'A': mod_file,
+  'U': mod_file,
+  'D': del_file
+}
+
+@pytest.yield_fixture
+def svn_workdir(svn_repo):
+  workdir = Workspace()
+  workdir.run("svn co {}".format(svn_repo.uri))
+  yield (workdir, svn_repo)
+  workdir.teardown()
+
+
+@pytest.fixture
+def mock_repo(svn_workdir):
+  workdir, repo = svn_workdir
+  repo_name = str(repo.workspace.name)
+  co_dir = workdir.workspace / repo_name
+
+  for commit, _ in mock_repo_commits:
+    commit_msg = []
+    for action, filename in commit:
+      commit_msg.append(repo_actions[action](workdir, repo_name, filename))
+    workdir.run("svn ci -m \"{}\"".format("\\n".join(commit_msg)), cd=co_dir)
+  return (workdir, co_dir)
+
+
+def test_sibis_svn_update(mock_repo):
+  mock_ws, co_dir = mock_repo
+  mock_ws.run("svn up -r0", cd=co_dir)
+
+  client = SibisSvnClient(co_dir)
+  assert client, "Client should not be None"
+
+  for rev, (commit, diff) in enumerate(mock_repo_commits, 1): 
+    changes = client.update(revision=rev)
+    assert changes, "Changes should not be None"
+    set_diff = changes.actions.difference(commit)
+    assert changes.revision == rev, "Expected revision {}, got: {}".format(rev, changes.revision)
+    assert changes.target == '.', "Expected target of . for rev {}, got: {}".format(rev, changes.target)
+    assert diff == set_diff, "Change actions for rev {} expected {}, difference {}".format(rev, repr(commit), repr(set_diff))
+
+
+
+
+
