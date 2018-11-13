0a0bd8c68e497783a9b11545310787e8e4ae4915
Jim Klo <jim.klo@sri.com>
2018-11-12 17:13:20 -0800

    Fixed a problem when svn cli output contained warnings and other meta info. - capture but ignore errors and warnings - ignore summary information

--- svn.py
+++ svn.py
@@ -8,6 +8,19 @@ from __future__ import absolute_import, print_function
 from svn.local import LocalClient
 
 
+class UpdateActionTypes():
+  """
+  SVN 1.8 Update action types.
+  """
+  added = 'A'
+  broken_lock = 'B'
+  deleted = 'D'
+  updated = 'U'
+  conflicted = 'C'
+  merged = 'G'
+  existed = 'E'
+
+
 class SibisSvnUpdate():
   """
   Changelog status performed by an SVN update.
@@ -18,18 +31,6 @@ class SibisSvnUpdate():
     actions   (set(set)): The set of (action, filename) performed by the update.
   """
 
-  class ActionTypes():
-    """
-    SVN 1.8 Update action types.
-    """
-    added = 'A'
-    broken_lock = 'B'
-    deleted = 'D'
-    updated = 'U'
-    conflicted = 'C'
-    merged = 'G'
-    existed = 'E'
-
   def __init__(self, cmd_output=[]):
     """
     Constructor for SibisSvnUpdate.
@@ -39,9 +40,22 @@ class SibisSvnUpdate():
                               in the list represents one line of output.
     """
     if len(cmd_output) >= 2:
-      header = cmd_output[0]
-      body = cmd_output[1:-1]
-      footer = cmd_output[-1]
+      primary_content = []
+      self.warnings = []
+      self.errors = []
+      for l in cmd_output:
+        if l.startswith('Summary of '):
+          break
+        if l.startswith('svn: warning: '):
+          self.warnings.append(l[14:])
+        elif l.startswith('svn: error: '):
+          self.errors.append(l[12:])
+        else:
+          primary_content.append(l)
+      
+      header = primary_content[0]
+      body = primary_content[1:-1]
+      footer = primary_content[-1]
 
       ## get the file or folder updated
       ts = header.index(r"'")+1
