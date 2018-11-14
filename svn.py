0c838d5c705a2b1d6172f45c91ae2cd788a459f9
Jim Klo <jim.klo@sri.com>
2018-11-14 13:17:18 -0800

    add svn log and diff capabilities to sibispy.svn and increase test coverage.

--- svn.py
+++ svn.py
@@ -6,7 +6,10 @@
 
 from __future__ import absolute_import, print_function
 from svn.local import LocalClient
-
+from svn.exception import SvnException
+from collections import namedtuple
+import os
+import xml
 
 class UpdateActionTypes():
   """
@@ -21,17 +24,23 @@ class UpdateActionTypes():
   existed = 'E'
 
 
+UpdateAction_ = namedtuple('UpdateAction', ['action', 'path', 'kind'])
+class UpdateAction(UpdateAction_):
+  """
+  Action performed to a single file or dir as result of `svn update`
+  """
+  pass
+
 class SibisSvnUpdate():
   """
   Changelog status performed by an SVN update.
 
   Attributes:
-    target    (str):      The file or dirctory where svn update was performed.
-    revision  (int):      The current revision of the working directory
-    actions   (set(set)): The set of (action, filename) performed by the update.
+    target           (str): The file or dirctory where svn update was performed.
+    revision         (int): The current revision of the working directory
+    actions   (set(tuple)): The set of (action, filename) performed by the update.
   """
-
-  def __init__(self, cmd_output=[]):
+  def __init__(self, work_dir, cmd_output=[]):
     """
     Constructor for SibisSvnUpdate.
 
@@ -70,8 +79,55 @@ class SibisSvnUpdate():
       self.actions = set()
       for line in body:
         action, path = line.split(None, 1)
-        self.actions.add((action, path))
-      
+        abspath = os.path.join(work_dir, path)
+        if os.path.exists(abspath) and os.path.isdir(abspath):
+          kind = 'dir'
+        elif (os.path.exists(abspath) and os.path.isfile(abspath)):
+          kind = 'file'
+        else:
+          kind = 'none'
+        self.actions.add(UpdateAction(action, path, kind))
+
+
+class SibisSvnDiffPath():
+  class Kind():
+    dir = "dir"
+    file = "file"
+  
+  class Props():
+    none = "none"
+    modified = "modified"
+  
+  class Item():
+    none = "none"
+    added = "added"
+    modified = "modified"
+    deleted = "deleted"
+
+  def __init__(self, path, kind, item='none', props='none'):
+    self.path = path
+    self.kind = kind
+    self.item = item
+    self.props = props
+
+
+class SibisSvnDiff():
+  def __init__(self, summary_diff=""):
+    root = xml.etree.ElementTree.fromstring(summary_diff)
+
+    self.paths=[]
+    for path in root.iter('path'):
+      attrib = path.attrib
+      self.paths.append(SibisSvnDiffPath(path.text, **attrib))
+
+  def files_changed(self, include_deleted=False):
+    files_changed = []
+    for pth in self.paths:
+      if not include_deleted and pth.item == SibisSvnDiffPath.Item.deleted:
+        continue
+      if pth.kind == SibisSvnDiffPath.Kind.file:
+        files_changed.append(pth.path) 
+    return files_changed
 
 class SibisSvnClient(LocalClient):
   """
@@ -80,21 +136,38 @@ class SibisSvnClient(LocalClient):
 
   Assumes that the path provided is an existing svn working directory.
   """
-  
-  def __init__(self, path_, *args, **kwargs):
+  _DEFAULT_ENV = {
+    'LANG': 'C.UTF-8',
+    'LANGUAGE': 'C.UTF-8',
+    'LC_ALL': 'C.UTF-8'
+  }
+
+  def __init__(self, path_, env=_DEFAULT_ENV, *args, **kwargs):
     """
     Constructor for SibisSvnClient class.
 
     Parameters:
       path_     (str):  path to svn working directory
+      env      (dict):  [optional] dictionary of environment vars to add to svn process
       username  (str):  [optional] svn repository username
       password  (str):  [optional] svn repository password
     """
-    super(SibisSvnClient, self).__init__(path_, *args, **kwargs)
+    super(SibisSvnClient, self).__init__(path_, env=env, *args, **kwargs)
+
 
   def update(self, rel_filepaths=[], revision=None):
     """
     Update the current working directory to current revision.
+    
+    NOTE: Output parsing is unreliable, and svn update output is a bit unreliable. If you need a changelog
+    it is reccommended to perform something like this instead:
+
+    >>> client = SibisSvnClient('/path/to/workdir')
+    >>> info = client.info()
+    >>> start_rev = info['entry_revision']
+    >>> changes = client.update()
+    >>> diff = client.diff_path(start_rev)
+    >>> changed_files = diff.files_changed(True)
 
     Parameters:
       rel_filepaths (list(str)):    list of relative subpaths to update
@@ -110,4 +183,32 @@ class SibisSvnClient(LocalClient):
       'update',
       cmd,
       wd=self.path)
-    return SibisSvnUpdate(cmd_out)
+    return SibisSvnUpdate(self.path, cmd_out)
+  
+  def log(self, *args, **kwargs):
+    """
+    See svn.common.CommonClient.log_default for parameters.
+    """
+    return self.log_default(*args, **kwargs)
+
+  def diff_path(self, revision, rel_filepath=None):
+    cmd = ['--summarize', '--xml']
+    if revision is not None:
+      cmd += ['-r', str(revision)]
+    
+    if rel_filepath is not None:
+      work_dir = os.path.join(self.path, rel_filepath)
+    else:
+      work_dir = self.path
+    
+    try:
+      cmd_out = self.run_command('diff', cmd, wd=work_dir, do_combine=True)
+    except SvnException as e:
+      raise SibisSvnException(e)
+
+
+    return SibisSvnDiff(cmd_out)
+
+class SibisSvnException(Exception):
+  def __init__(self, *args, **kwargs):
+    super(SibisSvnException, self).__init__(*args, **kwargs)
