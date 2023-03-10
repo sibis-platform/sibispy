c15e7b1bb8ecdfef4612721c1c603131c48fdc01
kipohl <pohl.kilian@gmail.com>
2023-03-10 15:24:47 -0800

    Changed error output to utilize f-string to deprecated approach

--- cmds/change_complete_field_in_entry_and_import.py
+++ cmds/change_complete_field_in_entry_and_import.py
@@ -179,7 +179,7 @@ for form_prefix, form_name in forms.items():
     event_mapping =  event_mapping_tmp[event_mapping_tmp.str.startswith(args.event, na=False)].tolist()
 
     if len(event_mapping) == 0 : 
-        print("ERROR: Event name starting with '"+ args.event + "' for '" + form_name + "' could not be found !")
+        print(f"ERROR: Event name starting with {args.event} for {form_name} could not be found !")
         continue 
 
     fields_list = [complete_label,record_label,'visit_ignore']
