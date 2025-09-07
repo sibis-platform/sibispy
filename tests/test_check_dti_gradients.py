4fea0cf34b074cd3510d5dd5b9dbe3c659f4803d
kipohl <pohl.kilian@gmail.com>
2025-09-07 00:38:45 -0700

    more complete test

--- tests/test_check_dti_gradients.py
+++ tests/test_check_dti_gradients.py
@@ -46,12 +46,45 @@ def test_check_dti_gradients(session, slog):
     gt_path_dict = check.get_ground_truth_gradient_path_dict()
     assert(len(gt_path_dict))
  
-    # Make sure ground truth is defined 
-    check_stack_scn = next(iter(gt_path_dict))
-    scn_dic= gt_path_dict[check_stack_scn]
-    check_stack_mod = next(iter(scn_dic))
-    mod_dic= scn_dic[check_stack_mod]
-    check_stack_seq = next(iter(mod_dic))
-    check_stack_xml = glob.glob(mod_dic[check_stack_seq])
+    # Make sure ground truth is defined
+    
+    # go through scanner 
+    # check_stack_scn = next(iter(gt_path_dict))
+    print(" ")
+    for check_stack_scn in gt_path_dict.keys() :
+        print("Scanner:", check_stack_scn) 
+        scn_dic= gt_path_dict[check_stack_scn]
 
-    assert(check.check_diffusion('TEST','TEST',check_stack_xml,check_stack_scn,check_stack_mod,"",check_stack_seq))
+        # check_stack_mod = next(iter(scn_dic))
+        for check_stack_mod in  scn_dic.keys() :
+            # default is first one
+            print("   Mode:", check_stack_mod)
+            mod_dic= scn_dic[check_stack_mod]
+    
+            # By default whatever is first
+            # check_stack_seq = next(iter(mod_dic))
+            for check_stack_seq  in mod_dic.keys() :
+                print("     Seq:",  check_stack_seq,end=" ")
+                check_stack_xml = glob.glob(mod_dic[check_stack_seq])
+
+                # Currently not acquired for all settings
+                if "b3000" in check_stack_seq :
+                    if len(check_stack_xml ) < 2:
+                        print("\033[1mMissing\033[0m")
+                        continue
+
+                    # siemens acquires two scans that are named pe1 and pe2 - GE only one scans
+                    if  check_stack_scn == "SIEMENS": 
+                        if  check_stack_seq == "pe1-dti6b3000" :
+                            check_stack_xml = [check_stack_xml[0]]
+                        elif  check_stack_seq == "pe2-dti6b3000" :
+                            check_stack_xml = [check_stack_xml[1]]
+                    # print("===check_stack_xml", check_stack_xml)
+
+                assert(check.check_diffusion('TEST','TEST',check_stack_xml,check_stack_scn,check_stack_mod,"",check_stack_seq))
+                print("Passed")
+
+
+    
+    
+        
